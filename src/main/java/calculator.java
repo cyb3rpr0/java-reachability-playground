7a1c4cf44a74a1ce025b0932bfaeda17f244c053
gratt410 <gratt98@icloud.com>
2024-10-04 13:36:38 -0400

    modified pom.xml,and added new java files

---
+++ src/main/java/calculator.java
@@ -0,0 +1,13 @@
+public class calculator {
+    
+}
+public class Calculator {
+
+    public int addition(String arguments) {
+
+        int sum = 0;
+        for (String add : arguments.split("\\+"))
+            sum += Integer.valueOf(add);
+            return sum;
+    }
+}
